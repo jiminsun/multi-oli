a90b25dad24aff5ccbfae024e3a5a8630127b092
jiminsun <jiminxsun@gmail.com>
2020-11-26 16:32:45 +0900

    [Python] fixed exp name handling

--- utils.py
+++ utils.py
@@ -1,13 +1,12 @@
 from datetime import datetime
 
-SEP_CODE = "\u241E"
-
 
 def generate_exp_name(args):
     exp_name = f'{args.lang}/{args.task}/{args.bert}/'
-    now = datetime.now().strftime("%m-%d-%H:%M")
-    exp_name += now
     if len(args.exp_name):
-        exp_name = exp_name + f'_{args.exp_name}'
+        exp_name = exp_name + f'{args.exp_name}'
+    else:
+        now = datetime.now().strftime("%m-%d-%H:%M")
+        exp_name += now
     print(exp_name)
     return exp_name
