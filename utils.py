9675a15397537546a4389ece87b2cc954acc918d
jiminsun <jiminxsun@gmail.com>
2020-11-27 22:09:04 +0900

    [Python] added util functions

--- utils.py
+++ utils.py
@@ -1,4 +1,11 @@
+import os
+import re
 from datetime import datetime
+
+import pandas as pd
+
+TASK_IDX_TO_LABEL = {0: 'NOT', 1: 'OFF'}
+LAND_IDX_TO_LABEL = {0: 'da', 1: 'en'}
 
 
 def generate_exp_name(args):
@@ -10,3 +17,46 @@ def generate_exp_name(args):
         exp_name += now
     print(exp_name)
     return exp_name
+
+
+def parse_score(ckpt_path, metric='val_f1'):
+    score = re.search(metric + '=[0-9]+\.[0-9]+', ckpt_path).group()
+    score = float(re.search('[0-9]+\.[0-9]+', score).group())
+    return score
+
+
+def find_best_ckpt(fpath, metric='val_f1'):
+    ckpts = [os.path.join(fpath, c) for c in os.listdir(fpath) if metric in c]
+    ckpts = [(ckpt, parse_score(ckpt, metric)) for ckpt in ckpts]
+    if metric == 'val_f1':
+        # higest f1 score
+        best_ckpt = sorted(ckpts, key=lambda x: x[1])[-1][0]
+    elif metric == 'val_loss':
+        # smallest loss
+        best_ckpt = sorted(ckpts, key=lambda x: x[1])[0][0]
+    return best_ckpt
+
+
+def generate_output_name(args):
+    if args.load_from is not None:
+        model_dir, ckpt = os.path.split(args.load_from)
+        model_name, _ = os.path.splitext(ckpt)
+    output_name = f'results/{args.task}/{args.lang}_train_{model_name}.tsv'
+    return output_name
+
+
+def save_prediction(test_predictions, output_fname, args):
+    test_predictions = test_predictions[0]
+    output = pd.DataFrame({
+        'sample': test_predictions['samples'],
+        'label': test_predictions['y_true'],
+        'pred': test_predictions['y_pred']
+    })
+    output['label'] = output['label'].apply(lambda x: TASK_IDX_TO_LABEL[x])
+    output['pred'] = output['pred'].apply(lambda x: TASK_IDX_TO_LABEL[x])
+    if args.task == 'lang':
+        output['logit'] = test_predictions['lang_logits']
+        output['logit'] = output['logit'].apply(lambda x: float(x[0]))
+    output['lang'] = args.lang
+    output.to_csv(output_fname, sep='\t', encoding='utf-8')
+    print(f'Predicted outputs save as {output_fname}')
